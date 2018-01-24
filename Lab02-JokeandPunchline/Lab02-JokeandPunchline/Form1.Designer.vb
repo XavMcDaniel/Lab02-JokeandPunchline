12290f852bd4160334692c3a4c2c5dd2dd0fc504
Xavier McDaniel <S01051073@acad.tri-c.edu>
2018-01-24 14:18:50 -0500

    Finished Assignment

--- Lab02-JokeandPunchline/Lab02-JokeandPunchline/Form1.Designer.vb
+++ Lab02-JokeandPunchline/Lab02-JokeandPunchline/Form1.Designer.vb
@@ -22,9 +22,54 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(36, 108)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Setup"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 53)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(180, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Click the Setup button to hear a joke"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(149, 108)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Punch line"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(257, 158)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
 End Class
--- Lab02-JokeandPunchline/Lab02-JokeandPunchline/Form1.vb
+++ Lab02-JokeandPunchline/Lab02-JokeandPunchline/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "Why did the chewing gum cross the road?"
+    End Sub
 
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text = "Because it got stuck to the chicken’s foot."
+    End Sub
 End Class
